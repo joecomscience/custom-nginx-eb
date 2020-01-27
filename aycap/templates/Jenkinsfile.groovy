5c0e4938f04e0f7f340fb68a9ef6c52ec9c791aa
joewalker <assassin.joe@hotmail.com>
2020-01-27 16:20:21 +0700

    print current path

--- aycap/templates/Jenkinsfile.groovy
+++ aycap/templates/Jenkinsfile.groovy
@@ -4,12 +4,16 @@ def defaultPipeline(region=null, awsProfile=null, applicationId=null) {
     def authorName        = sh(script: "git --no-pager show -s --format='%an' HEAD", returnStdout: true).trim()
     
     stage('Install Dependencies') {
+      sh 'pwd'
       println("joewalker ${commitId}-${authorName}")
     }
 
     stage('Check quality') {
       sh """
-        /home/sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=spring-boot-demo -Dsonar.sources=src -Dproject.settings=./src/sonar.properties -Dsonar.host.url=http://host.docker.internal:9000
+        /home/sonar-scanner/bin/sonar-scanner \
+        -Dsonar.projectKey=spring-boot-demo \
+        -Dsonar.sources=src/main/java \
+        -Dsonar.host.url=http://host.docker.internal:9000
       """
     }
     stage('Unit Test') {}
